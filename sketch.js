3d4bbc38986773d0baec1fbab7ce4c997b2786f0
VishmayaGithub <drsbalupriya@gmail.com>
2020-10-19 17:41:20 +0530

    editted

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform1;
 
-function preload(https://github.com/whitehatjr/angryBirdsStage2.git) {
+function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
 
@@ -17,7 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-	platform = new Ground(150,305,300,150)
+	platform1 = new Ground(150,305,300,150)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -49,7 +49,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-    platform.display()
+	platform1.display()
 
     box3.display();
     box4.display();
